6fe1379fb6c7c185078a7d23da166f1d69282657
jm011jh <jm011jh@gmail.com>
2022-05-10 14:48:53 +0900

    sjm main done

--- web/easyweb/js/layout.js
+++ web/easyweb/js/layout.js
@@ -1,3 +1,4 @@
 $(window).ready(function(){
     $.get(`/sde_design/skin2/inc/header.html`,(data)=>{$("#header").prepend(data)})
+    $.get(`/sde_design/skin2/inc/footer.html`,(data)=>{$("#footer").prepend(data)})
 })
